4d780b69d565ff43d97d4b3a3c13c2182b09a250
KennyD4Christ <kennyd4christ@gmail.com>
2024-07-24 13:16:18 +0100

    6-final-user.js

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,19 +1,13 @@
-import signUpUser from './4-signup-user.js';
-import uploadPhoto from './5-upload-photo.js';
+import signUpUser from './4-user-promise.js';
+import uploadPhoto from './5-photo-reject.js';
 
-function handleProfileSignup(firstName, lastName, fileName) {
-  // Create promises for signUpUser and uploadPhoto
+export default function handleProfileSignup(firstName, lastName, fileName) {
   const signUpPromise = signUpUser(firstName, lastName);
-  const uploadPromise = uploadPhoto(fileName);
+  const uploadPhotoPromise = uploadPhoto(fileName);
 
-  // Return a promise that resolves with an array of results
-  return Promise.allSettled([signUpPromise, uploadPromise])
-    .then((results) => {
-      return results.map(result => ({
-	status: result.status,
-	value: result.status === 'fulfilled' ? result.value : result.reason
-      }));
-    });
+  return Promise.allSettled([signUpPromise, uploadPhotoPromise])
+    .then((results) => results.map((result) => ({
+      status: result.status,
+      value: result.status === 'fulfilled' ? result.value : result.reason,
+    })));
 }
-
-export default handleProfileSignup;
