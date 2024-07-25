f39e288e7177712b55aece0d6307ffa365d7135b
KennyD4Christ <kennyd4christ@gmail.com>
2024-07-25 14:44:17 +0100

    0-promise.js

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,6 +1,7 @@
-const getResponseFromAPI = () => ({
-  status: 200,
-  body: 'photo-profile-1',
-});
+const getResponseFromAPI = () => {
+  return new Promise((resolve) => {
+    resolve({ status: 200, body: 'photo-profile-1' });
+  });
+};
 
 export default getResponseFromAPI;
