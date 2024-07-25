b0501766986074350d13e946c9a7901f4417bf78
KennyD4Christ <kennyd4christ@gmail.com>
2024-07-25 14:00:57 +0100

    0-promise.js

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,11 +1,3 @@
-/**
- *  * Returns a promise that resolves to a response object.
- *   * @returns {Promise<{status: number, body: string}>}
- */
-const getResponseFromAPI = () => {
-  return new Promise((resolve) => {
-    resolve({ status: 200, body: 'photo-profile-1' });
-  });
-};
+const getResponseFromAPI = () => new Promise(resolve => resolve({ status: 200, body: 'photo-profile-1' }));
 
 export default getResponseFromAPI;
