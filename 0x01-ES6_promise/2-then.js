76897ab7cd37092adb5374b81694227885a023c2
KennyD4Christ <kennyd4christ@gmail.com>
2024-07-25 14:53:01 +0100

    2-then.js

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -5,7 +5,7 @@ function handleResponseFromAPI(promise) {
       console.log('Got a response from the API');
       return {
         status: 200,
-        body: 'success'
+        body: 'success',
       };
     })
     .catch(() => {
