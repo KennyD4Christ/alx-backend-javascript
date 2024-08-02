5e5b431122106f54800e7a4e87c412104bcabd9b
KennyD4Christ <kennyd4christ@gmail.com>
2024-08-02 10:07:49 +0100

    4-update_grade_by_city.js

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,8 +1,8 @@
 function updateStudentGradeByCity(students, city, newGrades) {
   return students
-    .filter(student => student.location === city)
+    .filter((student) => student.location === city)
     .map(student => {
-      const gradeObj = newGrades.find(grade => grade.studentId === student.id);
+      const gradeObj = newGrades.find((grade) => grade.studentId === student.id);
       return { ...student, grade: gradeObj ? gradeObj.grade : 'N/A' };
     });
 }
