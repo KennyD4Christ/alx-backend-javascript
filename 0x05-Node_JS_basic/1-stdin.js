a19ff16eab2f1fe21eda8dd06828fc2e502a3c80
KennyD <danieljameskehinde@yahoo.com>
2024-09-18 11:02:42 +0100

    1-stdin.js

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -5,13 +5,6 @@ console.log('Welcome to Holberton School, what is your name?');
 process.stdin.on('data', (data) => {
   const name = data.toString().trim();
   console.log(`Your name is: ${name}`);
-
-  // For piped input, display the closing message
-  if (!process.stdin.isTTY) {
-    console.log('This important software is now closing');
-  }
-
-  // Close the stdin stream to end the process properly
-  process.stdin.end();
+  console.log('This important software is now closing');
+  process.exit();
 });
-
