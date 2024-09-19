3af5809d87eafbe6f4643a3fa0db75bc4ee81aea
KennyD <danieljameskehinde@yahoo.com>
2024-09-19 09:04:50 +0100

    1-stdin.js

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -3,7 +3,7 @@
 console.log('Welcome to Holberton School, what is your name?');
 
 process.stdin.on('data', (data) => {
-  const name = data.toString().trim();
+  const name = data.toString().replace(/\n$/, '');
   console.log(`Your name is: ${name}`);
   console.log('This important software is now closing');
   process.exit();
