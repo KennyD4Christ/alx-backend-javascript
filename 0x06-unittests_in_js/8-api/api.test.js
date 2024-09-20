4118240a3a196e45c146312dd57310dc96c787d6
KennyD <danieljameskehinde@yahoo.com>
2024-09-20 12:00:42 +0100

    8-api

--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -1,26 +1,24 @@
-const request = require('request');
-const app = require('./api');
 const chai = require('chai');
 const chaiHttp = require('chai-http');
+const app = require('./api.js'); // Ensure this points to your api.js file
 
 chai.use(chaiHttp);
-const { expect } = chai;
 
 describe('Index page', () => {
-  it('Correct status code?', (done) => {
+  it('should return the correct status code', (done) => {
     chai.request(app)
       .get('/')
       .end((err, res) => {
-        expect(res).to.have.status(200);
+        chai.expect(res).to.have.status(200);
         done();
       });
   });
 
-  it('Correct result?', (done) => {
+  it('should return the correct result', (done) => {
     chai.request(app)
       .get('/')
       .end((err, res) => {
-        expect(res.text).to.equal('Welcome to the payment system');
+        chai.expect(res.text).to.equal('Welcome to the payment system');
         done();
       });
   });
